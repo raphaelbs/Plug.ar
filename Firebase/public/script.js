8c6440d007e634bb5149710abf83c38851db7be5
Raphael Brandão <raphael.b.souza@hotmail.com>
2017-04-16 02:16:32 -0300

    inserindo infraestrutura do projeto web

---
+++ Firebase/public/script.js
@@ -0,0 +1,26 @@
+angular.module('app', ['ui.router', 'ngMaterial', 'ngMessages']);
+
+angular.module('app').config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
+
+	$mdThemingProvider.theme('default')
+	.primaryPalette('indigo')
+	.accentPalette('red');
+
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	.state('home', {
+		url: '/',
+		views : {
+			'main' : {
+				templateUrl : '/views/home',
+				controller : 'home'
+			}
+		}
+	});
+}]);
