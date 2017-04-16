bcfb163cf398e4b0d324ec7db1974f52bfb7f87e
Raphael Brandão <raphael.b.souza@hotmail.com>
2017-04-16 03:06:08 -0300

    inserindo logistica de login

--- Firebase/public/script.js
+++ Firebase/public/script.js
@@ -24,3 +24,58 @@ angular.module('app').config(['$stateProvider', '$urlRouterProvider', '$location
 		}
 	});
 }]);
+
+angular.module('app').controller('app', ['$scope', function($scope){
+	var gProvider = new firebase.auth.GoogleAuthProvider();
+
+	$scope.session = function(){
+		if($scope.user){
+			firebase.auth().signOut().then(function() {
+				delete $scope.user;
+			}, function(error) {
+				console.error(error);
+			});
+			return;
+		}
+		firebase.auth().signInWithPopup(gProvider).then(function(result) {
+			var token = result.credential.accessToken;
+			$scope.user = result.user;
+			console.log(result);
+		}).catch(function(error) {
+			console.error(error);
+		});
+	};
+
+	$scope.color = { color: "'rgba(0,0,0,0;26)'", caption: 'carregando...'};
+
+}]);
+
+
+/*global angular:true*/
+/**
+ * [brDate]
+ * Filtro para regularizar as datas no modo brasileiro.
+ * Ex: {{123123 | brDate}} = 01/02/2017
+ */
+angular.module('app').filter('brDate', function() {
+	function doDate(long){
+		var d = new Date(long);
+		if(typeof d == 'string') return '*data-invalida*';
+		var diff = new Date() - d;
+		if(diff<60000) return 'segundos atrás';
+		if(diff<120000) return '1 minuto atrás';
+		if(diff<3600000) return Math.round(diff/60000) + ' minutos atrás';
+		if(diff<7200000) return '1 hora atrás';
+		if(diff<86400000) return Math.round(diff/3600000) + ' horas atrás';
+		var o = (d.getDate() < 10 ? '0' : '') + d.getDate() + '/';
+		o += (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + '/';
+		o += d.getFullYear();
+		return o;
+	}
+	return function(input) {
+		if(input == undefined) return '*sem-data*';
+		if(input.match(/^\d+$/g)) return doDate(input);
+		if(input.match(/^\w{24,24}$/g)) return doDate(parseInt(input.substring(0, 8), 16)*1000);
+		return doDate(input);
+	};
+});
