7e7e549194f9f3c8c94135c424fb9c6c4fa716c7
Raphael Brandão <raphael.b.souza@hotmail.com>
2017-05-28 19:53:12 -0300

    Update ESP8266.ino

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,17 +1,14 @@
 #include "WiFinder.h"
 #include <FirebaseArduino.h>
 
-#define FIREBASE_HOST "plugar-e4795.firebaseio.com"
-#define FIREBASE_AUTH "lgshhtREzTrtdGzHEV5jyAzmC16SlTg0ndgVYQuk"
+#define FIREBASE_HOST "YOUR_FIREBASE_HOST.firebaseio.com"
+#define FIREBASE_AUTH "YOUR_FIREBASE_AUTH"
 #define LED_PIN 10
 #define BTN_PIN 2
 
 char* WIFIS[][2] = {
-  {"Private", "666conexaoprivate"},
-  {"rapha", "rapha123"},
-  {"livs", "asdf1234"},
-	{"Find", "find@2016"},
-	{"Aja", "0135792468aja"}  
+  {"Wifi1", "wifi1pass"},
+  {"Wifi2", "wifi2pass"}
 };
 WiFinder wifinder(WIFIS);
 
@@ -25,23 +22,23 @@ StaticJsonBuffer<250> nodemcuBuffer;
 JsonObject& nodemcuObject = nodemcuBuffer.createObject();
 
 void setup() {
-	Serial.begin(115200);
+  Serial.begin(115200);
   wifinder.setLed(LED_PIN);
-	wifinder.connect();
-	pinMode(BTN_PIN, INPUT_PULLUP);
-	Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  wifinder.connect();
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.setString("localIp", wifinder.getLocalIp());
-	attachInterrupt(BTN_PIN, readBtn, FALLING);
+  attachInterrupt(BTN_PIN, readBtn, FALLING);
   initJSONs();
 }
 
 void loop() {
-	wifinder.handle();
-	readLEDFirebase();
-	updateFirebase();
-	readDelayFirebase();
+  wifinder.handle();
+  readLEDFirebase();
+  updateFirebase();
+  readDelayFirebase();
 
-	delay(fb_delay);
+  delay(fb_delay);
 }
 
 // Inicializa conteudo statico dos jsons
@@ -54,44 +51,44 @@ void initJSONs(){
 
 // Lê do firebase: LED
 void readLEDFirebase(){
-	bool cled_status = Firebase.getBool("LED");
-	if(led_status == cled_status) return;
-	led_status = cled_status;
-	Serial.print("LED alterado para: ");
-	Serial.println(led_status);
-	digitalWrite(LED_PIN, led_status);
+  bool cled_status = Firebase.getBool("LED");
+  if(led_status == cled_status) return;
+  led_status = cled_status;
+  Serial.print("LED alterado para: ");
+  Serial.println(led_status);
+  digitalWrite(LED_PIN, led_status);
 }
 
 // Lê do firebase: Delay
 void readDelayFirebase(){
-	fb_delay = Firebase.getInt("delay");
-	if(fb_delay < 1000) fb_delay = 1000;
-	if(fb_delay == prev_delay) return;
-	Serial.print("Delay alterado para: ");
-	Serial.println(fb_delay);
-	prev_delay = fb_delay;
+  fb_delay = Firebase.getInt("delay");
+  if(fb_delay < 1000) fb_delay = 1000;
+  if(fb_delay == prev_delay) return;
+  Serial.print("Delay alterado para: ");
+  Serial.println(fb_delay);
+  prev_delay = fb_delay;
 }
 
 // Atualiza do firebase: LED
 void updateFirebase(){
-	if(!alterado) return;
+  if(!alterado) return;
   bool ls = !led_status;
-	Firebase.setBool("LED", ls);
-	Serial.print("Definindo LED no firebase: ");
-	Serial.println(!led_status);
-	if (Firebase.failed()) {
-		Serial.print("Erro ao setar LED:");
-		Serial.println(Firebase.error());
-		return;
-	}
+  Firebase.setBool("LED", ls);
+  Serial.print("Definindo LED no firebase: ");
+  Serial.println(!led_status);
+  if (Firebase.failed()) {
+    Serial.print("Erro ao setar LED:");
+    Serial.println(Firebase.error());
+    return;
+  }
   nodemcuObject["action"] = ls;
   Firebase.push("users", nodemcuObject);
-	alterado = false;
-	attachInterrupt(BTN_PIN, readBtn, FALLING);
+  alterado = false;
+  attachInterrupt(BTN_PIN, readBtn, FALLING);
 }
 
 void readBtn(){
-	Serial.println("Alterando LED via interrupt");
-	alterado = true;
-	detachInterrupt(BTN_PIN);
+  Serial.println("Alterando LED via interrupt");
+  alterado = true;
+  detachInterrupt(BTN_PIN);
 }
